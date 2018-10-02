a201e1264b57a23b3bf4dfa3825b3f1482509d98
FireTechStudios <laidlawawz@gmail.com>
2018-10-02 08:01:12 +0100

    Better lvl1

--- Destruction Game/Assets/Scripts/PlayerMovement.cs
+++ Destruction Game/Assets/Scripts/PlayerMovement.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public Rigidbody player;
     public float originalSpeed;
     private float speed;
+    public float maxSpeed = 20f;//Replace with your max speed
 
     private void Start()
     {
@@ -15,12 +17,17 @@ public class PlayerMovement : MonoBehaviour {
 
     void FixedUpdate()
     {
-        var moveDirection = new Vector3(Input.GetAxisRaw("Horizontal")/2, 0, Input.GetAxisRaw("Vertical"));
+        var moveDirection = new Vector3(Input.GetAxisRaw("Horizontal") / 2, 0, Input.GetAxisRaw("Vertical"));
         moveDirection = Camera.main.transform.TransformDirection(moveDirection);
         moveDirection.y = 0;
 
 
-        player.MovePosition(player.position + moveDirection * speed * Time.deltaTime);
-    }
+        //player.MovePosition(player.position + moveDirection * speed * Time.deltaTime);
+        player.AddForce(moveDirection * speed);
 
+        if (player.velocity.magnitude > maxSpeed)
+        {
+            player.velocity = player.velocity.normalized * maxSpeed;
+        }
+    }
 }
